a9ecd2792578d02284c1220dbf9359ff365b71ec
Malyshev Artem <proofit404@gmail.com>
2014-09-25 13:27:40 +0400

    Init repo.

---
+++ company-edbi.el
@@ -0,0 +1,6 @@
+(add-hook 'edbi:dbview-update-hook 'edbi:ac-editor-word-candidate-update)
+
+edbi:ac-editor-table-candidates
+edbi:ac-editor-column-candidates
+edbi:ac-editor-type-candidates
+edbi:ac-editor-keyword-candidates
